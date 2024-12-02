f49fd599250dd92670cc6d5156f1ac8f35a38896
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2024-12-02 14:45:57 -0800

    01.2

--- 01/01.py
+++ 01/01.py
@@ -19,4 +19,12 @@ def part1(l1: list, l2: list) -> int:
         sum += abs(sorted_l1[i] - sorted_l2[i])
     return sum
 
-print(part1(l1, l2))
+def part2(l1: list, l2: list) -> int:
+    ans = 0
+    unique_l1 = set(l1)
+    for i in unique_l1:
+        ans += i * l2.count(i)
+    return ans
+
+print("Part 1:", part1(l1, l2))
+print("Part 2:", part2(l1, l2))
