0eaa700273e81640ed966f9345be71ec2392a635
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2024-12-02 15:59:36 -0800

    02.2

--- 02/02.py
+++ 02/02.py
@@ -24,4 +24,27 @@ def part1(levels: list) -> int:
                 ans += 1
     return ans
 
+def is_safe(l: list) -> bool:
+    if (l != sorted(l) and l != sorted(l, reverse=True)):
+        return False
+    for i in range(len(l)-1):
+        diff = abs(l[i+1] - l[i])
+        if diff != 1 and diff != 2 and diff != 3:
+            return False
+    return True
+
+def part2(levels: list) -> int:
+    ans = 0
+    for level in levels:
+        safe = False
+        for j in range(len(level)):
+            alt_level = level[:j] + level[j+1:]
+            if is_safe(alt_level):
+                safe = True
+                break
+        if safe:
+            ans += 1
+    return ans
+
 print("Part 1:", part1(levels))
+print("Part 2:", part2(levels))
