951e9aabfe5d337647c0559a603888765534c8b2
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2024-12-02 22:19:56 -0800

    03.2

--- 03/03.py
+++ 03/03.py
@@ -17,4 +17,23 @@ def part1(matches):
         ans += num1*num2
     return ans
 
+adv_pattern = r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)"
+matches2 = re.findall(adv_pattern, str)
+
+def part2(matches):
+    ans = 0
+    active = True
+    for match in matches:
+        #print(match)
+        if match == "do()":
+            active = True
+        elif match == "don't()":
+            active = False
+        else:
+            if active:
+                num1, num2 = map(int, re.findall(r"\d{1,3}", match))
+                ans += num1*num2
+    return ans
+
 print("Part 1:", part1(matches))
+print("Part 2:", part2(matches2))
